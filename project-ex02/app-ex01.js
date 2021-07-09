a05ccb137e5ab7cccc82fb7b5f0cce38bfa88159
unknown <mani703@naver.com>
2021-07-09 12:53:38 +0900

    모듈 테스트

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,9 @@
+/**
+* mani703-math.js 파일 모듈 테스트
+*/
+
+const mnMath = require('../mani703-math/mani703-math');
+
+console.log(mnMath.sum(1, 2, 3, 4));
+console.log(mnMath.max(-10, -20, -30, -40));
+console.log(mnMath.max(-10, -2, 0, 1, 2));
